2fdb4ee9d16536f4bd0330a2d3490f8dbf5bd5b2
Vincent <chridtinewanja@gmail.com>
2020-11-13 09:43:33 +0300

    This adds the login and registration components

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+import { userInfo, UserInfo } from 'os';
+import { EventEmitter } from 'protractor';
+import {User} from './../user';
 
 @Component({
   selector: 'app-register',
@@ -6,7 +9,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  newUser: User = new User(0,"","","");
+  
 
+  createUser(){
+    this.newUser = new User(0,'','','')
+  }
   constructor() { }
 
   ngOnInit(): void {
