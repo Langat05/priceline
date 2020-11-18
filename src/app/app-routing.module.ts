548ccc5e7b937c58d742fbe8bf1dba234ea192d1
langat justus <justuslangat78@gmail.com>
2020-11-18 10:22:35 +0300

    modified the register component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { LoginComponent } from './login/login.component';
 import { VendorComponent } from './vendor/vendor.component';
 import { PaymentComponent } from './payment/payment.component';
 import { CategoriesComponent } from './categories/categories.component';
-
 import { RegisterComponent } from './register/register.component';
 import { ProfileComponent } from './profile/profile.component';
 import { HomeComponent } from './home/home.component';
@@ -11,7 +10,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent},
+  { path: '', component: HomeComponent},
   { path: 'profile', component: ProfileComponent},
   { path: 'register', component: RegisterComponent},
   { path: 'payment', component: PaymentComponent},
